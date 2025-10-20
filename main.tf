23a59e339da2c64be4f1f28bdf7bbd207fb7c929
Chijiokeproject <alaukwu.chijioke@cloudhight.com>
2025-10-21 00:58:48 +0100

    nexus

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ module "vpc" {
 }
 
 module "ansible" {
-  source    = "./module/ansible"
-  name      = var.project_name
-  vpc       = module.vpc.vpc_id
-  subnet_id = module.vpc.pub_sub1_id
-  keypair_name   = var.keypair_name
-  private_key = var.private_key
-  nexus_ip  = var.nexus_ip
-  nr_key    = var.nr_key
-  nr_acc_id = var.nr_acc_id
-  bastion_sg = module.bastion.bastion_sg_id
+  source                = "./module/ansible"
+  name                  = var.project_name
+  vpc                   = module.vpc.vpc_id
+  subnet_id             = module.vpc.pub_sub1_id
+  keypair_name          = var.keypair_name
+  private_key           = var.private_key
+  nr_key                = var.nr_key
+  nr_acc_id             = var.nr_acc_id
+  bastion_sg            = module.bastion.bastion_sg_id
+  nexus_ip              = module.nexus.nexus_public_ip
 }
 
 module "bastion"{
@@ -38,7 +38,19 @@ module "bastion"{
   nr_key                 = var.nr_key
   nr_acc_id              = var.nr_acc_id
   region                 = var.region
+ 
  }
+
+module "nexus" {
+  source              = "./module/nexus"
+  name                = var.name
+  vpc_id              = module.vpc.vpc_id
+  subnet_id           = module.vpc.pub_sub1_id
+  subnet_ids          = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
+  key_name            = module.vpc.public_key
+  domain              = var.domain
+  aws_acm_certificate    = "arn:aws:acm:us-west-1:123456789012:certificate/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
 
 
 
